2cbda6ff2273941505b8e307cebdc2b46141b8e5
ananyabs <ananyabs171@gmail.com>
2022-12-07 23:54:39 +0530

    added welcome note

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-
-
+@app.route('/welcome', methods=['GET'])
+def welcome_note():
+    return jsonify('Welcome to Flask app')
 
 if __name__=='__main__':
     app.run()
