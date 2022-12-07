bf575b14015e102ecfb7f4bd82e28ff46d77c052
ananyabs <ananyabs171@gmail.com>
2022-12-08 00:41:56 +0530

    added create query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from db import create, get
 
 
 app = Flask(__name__)
@@ -7,6 +8,18 @@ app = Flask(__name__)
 @app.route('/welcome', methods=['GET'])
 def welcome_note():
     return jsonify('Welcome to Flask app')
+
+@app.route('/addEmployee', methods=['POST'])
+def addEmployee():
+    try:
+        data = request.get_json()
+        queryVal = f"""INSERT INTO employees (name, email, position) VALUES(%s, %s, %s), ({data['name']}, {data['email']}, {data['position']})"""
+        create(queryVal)
+        return jsonify({"msg": "Employee Added Successfully"}), 200
+    except:
+        return jsonify({"msg": "Error"}), 400
+
+
 
 if __name__=='__main__':
     app.run()
