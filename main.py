fffa72c9057076f8b7d7114c543b5af84fa497e3
ananyabs <ananyabs171@gmail.com>
2022-12-08 01:09:37 +0530

    getting all employees

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def welcome_note():
     return jsonify('Welcome to Flask app')
 
+# adding employees
 @app.route('/addEmployee', methods=['POST'])
 def addEmployee():
     try:
@@ -17,6 +18,17 @@ def addEmployee():
         return jsonify({"msg": "Employee Added Successfully"}), 200
     except:
         return jsonify({"msg": "Error"}), 400
+
+
+# getting all employees
+@app.route('/list', methods=['GET'])
+def getAllEmployees():
+    try:
+        query = 'SELECT * FROM employees'
+        db_result = get(query)
+        return jsonify({"body": db_result, "msg": "Successfully listed"}), 200
+    except:
+        return jsonify({"msg": "Listing employees failed"}), 400
 
 
 
