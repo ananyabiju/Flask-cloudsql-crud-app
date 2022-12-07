53e21b78d7c5fc0648d8d58e9161d6153eee08c6
ananyabs <ananyabs171@gmail.com>
2022-12-07 23:31:10 +0530

    initial commit

---
+++ db.py
@@ -0,0 +1,43 @@
+import os
+import pymysql
+from flask import jsonify
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+    unix_socket = f'/cloudsql/{db_connection_name}'
+    try:
+        if os.environ.get('GAE_ENV') == 'standard':
+            conn = pymysql.connect(user=db_user,
+                password=db_password,
+                unix_socket=unix_socket,
+                db=db_name,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+    except pymysql.MySQLError as e:
+        return e
+    return conn
+
+
+def get(query):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        queryData = cursor.execute(query)
+        result = cursor.fetchall()
+        if(queryData > 0):
+            return jsonify(result)
+        else:
+            return "Oops! No data available"
+
+
+def create(query):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+    conn.commit()
+    conn.close()
